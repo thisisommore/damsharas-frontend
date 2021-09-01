a50e1c56ed25f2309d4d55c88b2ff9a968dcaf32
thisisommore <ommore501@gmail.com>
2021-09-01 08:14:46 +0000

    Introduce styled button

---
+++ src/components/StyledButton.tsx
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+const StyledButton = styled.button`
+  background-color: var(--color);
+  color: white;
+  border: none;
+  border-radius: 46px;
+  padding: 10px 20px;
+  font-family: "Acme";
+`;
+export default StyledButton;
