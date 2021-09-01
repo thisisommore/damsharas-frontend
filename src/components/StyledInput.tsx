6461d3e49a42a7e521b8cd17b0816b7459df3b37
thisisommore <ommore501@gmail.com>
2021-09-01 08:43:05 +0000

    StyledInput: Add box shadow on focus and error

--- src/components/StyledInput.tsx
+++ src/components/StyledInput.tsx
@@ -5,6 +5,19 @@ const StyledInput = styled.input`
   border: none;
   border-radius: 37px;
   padding: 20px;
+  transition: 0.3s ease-out;
+
+  border: 1px solid rgba(236, 236, 236, 0);
+
+  :focus {
+    border: 1px solid var(--color);
+    box-shadow: 0 0 10px var(--color);
+    outline: none;
+  }
+  &.error {
+    border: 1px solid red;
+    box-shadow: 0 0 10px red;
+  }
 
   ::placeholder {
     font-family: "Acme";
