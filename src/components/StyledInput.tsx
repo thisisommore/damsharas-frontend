2bd03d1627d364fd8e725322ec6cd08f8cb7036f
thisisommore <ommore501@gmail.com>
2021-09-01 08:11:57 +0000

    Introduce styled input

---
+++ src/components/StyledInput.tsx
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+const StyledInput = styled.input`
+  background-color: #ececec;
+  border: none;
+  border-radius: 37px;
+  padding: 20px;
+
+  ::placeholder {
+    font-family: "Acme";
+  }
+`;
+export default StyledInput;
