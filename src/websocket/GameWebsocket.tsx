dd37666be50c5a09a73544d5cce69c83de72f087
thisisommore <ommore501@gmail.com>
2021-09-03 04:06:55 +0000

    Add web sockets

---
+++ src/websocket/GameWebsocket.tsx
@@ -0,0 +1,42 @@
+import { io, Socket } from "socket.io-client";
+import { Player } from "../types/Player";
+
+var socket: Socket;
+socket = io("ws://localhost:3000");
+type Callback = (...args: any[]) => void;
+export function getName() {
+  return localStorage.getItem("name");
+}
+export function setName(name: string) {
+  return localStorage.setItem("name", name);
+}
+
+export function addHints(hints: string[]) {
+  console.log("hii");
+
+  socket.emit("addHints", hints);
+}
+
+export function register(type: Player) {
+  socket.emit("register", type);
+}
+
+export const listenToHints = (callback: Callback) => {
+  socket.on("hintsAdded", callback);
+};
+
+export function listenToNewFinder(callback: Callback) {
+  socket.on("finderConnected", callback);
+}
+
+export function listetToNewHelper(callback: Callback) {
+  socket.on("helperConnected", callback);
+}
+
+export function listenToKeywordGeneration(callback: Callback) {
+  socket.on("keywordGenerated", callback);
+}
+
+export function listenToHintsGeneration(callback: Callback) {
+  socket.on("hintsGenerated", callback);
+}
